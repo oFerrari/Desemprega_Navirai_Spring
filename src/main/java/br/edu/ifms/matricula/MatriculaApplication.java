9bfc730dc6cbb055143382b41a0affd2346fbeaa
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-03-15 20:30:47 -0400

    Adicionando Response ao EstudanteController

--- src/main/java/br/edu/ifms/matricula/MatriculaApplication.java
+++ src/main/java/br/edu/ifms/matricula/MatriculaApplication.java
@@ -1,5 +1,4 @@
 package br.edu.ifms.matricula;
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 import org.springframework.boot.SpringApplication;
--- src/main/java/br/edu/ifms/matricula/controller/EstudanteController.java
+++ src/main/java/br/edu/ifms/matricula/controller/EstudanteController.java
@@ -11,14 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifms.matricula.controller.dto.EstudanteRequest;
 import br.edu.ifms.matricula.controller.dto.EstudanteResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/estudante")
+@Tag(name = "Aluno", description = "Gerenciamento de Estudantes")
 public class EstudanteController {
 	@GetMapping()
 	public ResponseEntity<String> olaMundo() {
 		return ResponseEntity.ok("Ola Mundo");
 	}
+	
+	@Operation(summary = "Novo recurso", description = "Serviço para cadastrar um recurso")
+	  @ApiResponses(value = {
+	      @ApiResponse(responseCode = "200", description = "Operação de sucesso",
+	          content = @Content(mediaType = "application/json", schema = @Schema(implementation = EstudanteResponse.class))),
+	      @ApiResponse(responseCode = "500", description = "Falha no serviço", content = @Content)
+	  })
 
 //	https://reqbin.com/
 
