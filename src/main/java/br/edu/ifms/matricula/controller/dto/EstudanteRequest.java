21a5b99f27e7f6d8fae19a79bd066e0cf16002cb
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-03-29 20:05:11 -0400

    Adicionando validações ao EstudanteRequeste

--- src/main/java/br/edu/ifms/matricula/controller/dto/EstudanteRequest.java
+++ src/main/java/br/edu/ifms/matricula/controller/dto/EstudanteRequest.java
@@ -1,10 +1,25 @@
 package br.edu.ifms.matricula.controller.dto;
 
-public class EstudanteRequest {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+public class EstudanteRequest {
+	@Size(max = 80, message = "Informe o nome completo")
+	@NotBlank
 	private String nome;
+	
+	@NotBlank
+	@Size(min = 11)
 	private String cpf;
+	
+	@Size(max = 150, message = "Informe um endereço de e-mail")
+	@NotBlank
+	@Email
 	private String email;
+	
+	@Size(max = 50, message = "Informe sua senha com ate 50 caracteres")
+	@NotBlank
 	private String senha;
 
 	public String getNome() {
